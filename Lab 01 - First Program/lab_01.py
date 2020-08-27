a4cc7111719488f5b2275492bc89c983ae5e6f5c
Keith <keith.dittmer@my.simpson.edu>
2020-08-27 15:34:56 -0500

    Work on lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,7 @@
-hi
+print("I'm very excited for this class. I can't wait to learn how to make complex graphics")
+print("I have done a little python before but it was simple things like: \naddition and subtraction \nprint statements "
+      "\nand text based games.")
+print("I'm also very excited to learn more about python because I didn't know that it was so prevalent in the workplace"
+      ".")
+print("It reminds me of the quote \"You practice how you play\" \n\t So it's good to be practicing on the same type of "
+      "program")
---
+++ Testing/test.py
@@ -0,0 +1,5 @@
+print("""You can print
+on Multiple 
+lines using
+triple
+quotes""")
